01d403a26ab4c611c6a480af50f5f7aedb4030de
Marcos Moura <mrufino@ciandt.com>
2015-12-17 17:51:55 -0200

    Fix build

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 // support for Browserify
 
-require('./.temp/templates.js');
-require('./app/md-data-table/md-data-table.js');
-require('./app/md-data-table/scripts/mdColumnHeader.js');
-require('./app/md-data-table/scripts/mdDataTable.js');
-require('./app/md-data-table/scripts/mdDataTableCell.js');
-require('./app/md-data-table/scripts/mdDataTableFoot.js');
-require('./app/md-data-table/scripts/mdDataTableHead.js');
-require('./app/md-data-table/scripts/mdDataTablePagination.js');
-require('./app/md-data-table/scripts/mdDataTableProgress.js');
-require('./app/md-data-table/scripts/mdDataTableRow.js');
-require('./app/md-data-table/scripts/mdDataTableService.js');
-require('./app/md-data-table/scripts/mdSelectAll.js');
-require('./app/md-data-table/scripts/mdSelectRow.js');
+require('./dist/md-data-table.js');
 
 module.exports = 'md.data.table';
